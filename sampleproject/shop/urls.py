2164c47492107ff588fee360cd1a50ae18d94e4e
behnam alam <b_alamshahi@outlook.com>
2025-03-12 19:18:31 +0330

    first front of Add Admin Html without config scss

--- sampleproject/shop/urls.py
+++ sampleproject/shop/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import *
 
 
 urlpatterns = [
-  path("",index,name="indexpage")
+  path("",index,name="indexpage"),
+  path("add-admin",add_admin,name="add_admin")
 ]
--- sampleproject/shop/views.py
+++ sampleproject/shop/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
   return HttpResponse("hello world this is index page")
+
+def add_admin(request):
+  return render(request,"shop/add-admin.html")
